aff6db11f16908df91fd8032b018905c6fa1e2ac
vinibraaga <vinibraaga@gmail.com>
2021-01-02 20:12:30 -0300

    Formulario de criação de post

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -3,13 +3,19 @@ package com.spring.codeblog.controller;
 import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -33,5 +39,20 @@ public class CodeblogController {
 		Post post = codeblogService.findById(id);
 		mv.addObject("post", post);
 		return mv;
+	}
+	
+	@GetMapping("/newpost")
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@PostMapping("/newpost")
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()){
+			return "redirect:/newpost";
+		}
+		post.setData(LocalDate.now());
+		codeblogService.save(post);
+		return "redirect:/posts";
 	}
 }
