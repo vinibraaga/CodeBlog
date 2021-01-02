38a6a8d135802ec87d4010035af07ae4e1295ddf
vinibraaga <vinibraaga@gmail.com>
2021-01-02 18:04:21 -0300

    Layout base do blog

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -4,6 +4,7 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,7 +17,8 @@ public class CodeblogController {
 	@Autowired
 	CodeblogService codeblogService;
 	
-	@RequestMapping(value = "/posts", method = RequestMethod.GET)
+	//@RequestMapping(value = "/posts", method = RequestMethod.GET)
+	@GetMapping("/posts")
 	public ModelAndView getPosts() {
 		ModelAndView mv = new ModelAndView("posts");
 		List<Post> posts = codeblogService.findAll();
